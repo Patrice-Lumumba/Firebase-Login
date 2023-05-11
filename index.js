b358b344c1b5bbbb7b5c24747503a005c95afcea
Patrice Lumumba <Wapalu2004@gmail.com>
2023-05-11 21:11:46 +0200

    Ajout des fichiers

---
+++ index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAcjgxOdPwPQO3jjxL4HhbykYBuwUuphzU",
+    authDomain: "babillard-app-af8fb.firebaseapp.com",
+    projectId: "babillard-app-af8fb",
+    storageBucket: "babillard-app-af8fb.appspot.com",
+    messagingSenderId: "791019185506",
+    appId: "1:791019185506:web:cb02b58dc71c6bbdc4783b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
